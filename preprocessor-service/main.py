03b45987fe3a17589122dbe7751afe93c427cc21
Ujjwol Dandekhya <gunner04ujjwol@gmail.com>
2024-02-21 15:46:46 +0200

    Refactor preprocess service

--- preprocessor-service/main.py
+++ preprocessor-service/main.py
@@ -24,14 +24,13 @@ NETWORK_CONNECTION_EVENT = "network_connection"
 EVENT_TYPES = {NEW_PROCESS_EVENT, NETWORK_CONNECTION_EVENT}
 
 config = Config(
-    region_name = 'eu-west-1',
-    retries = {
-        'max_attempts': 3,
-        'mode': 'standard'
-    }
+    region_name="eu-west-1", retries={"max_attempts": 3, "mode": "standard"}
 )
 
-def receive_submissions(sqs_client, queue_url, batch_size, visibility_timeout):
+
+def receive_submissions(
+    sqs_client, queue_url: str, batch_size: int, visibility_timeout: int
+):
     """
     Receive messages from SQS
 
@@ -46,27 +45,28 @@ def receive_submissions(sqs_client, queue_url, batch_size, visibility_timeout):
     """
     try:
         response = sqs_client.receive_message(
-        QueueUrl=queue_url,
-        MaxNumberOfMessages=batch_size,
-        VisibilityTimeout=visibility_timeout
+            QueueUrl=queue_url,
+            MaxNumberOfMessages=batch_size,
+            VisibilityTimeout=visibility_timeout,
         )
-        messages = response.get('Messages', [])
+        messages = response.get("Messages", [])
         return messages
     except (EndpointConnectionError, ClientError) as e:
         pass
 
-def validate_submission(submission_data):
+
+def validate_submission(submission_data: dict):
     """
     Validates the submission data.
-    
+
     Args:
     submission_data: Decoded submission data
-    
+
     Returns:
     True if the submission is valid, False otherwise.
     """
     required_fields = ["submission_id", "device_id", "time_created", "events"]
-    
+
     # Check if all required fields are present
     if not all(field in submission_data for field in required_fields):
         print("Missing required fields")
@@ -74,11 +74,15 @@ def validate_submission(submission_data):
         return False
 
     # Check data types of required fields
-    if not isinstance(submission_data["submission_id"], str) or not is_valid_uuid(submission_data["submission_id"]):
+    if not isinstance(submission_data["submission_id"], str) or not is_valid_uuid(
+        submission_data["submission_id"]
+    ):
         print("Detected invalid submission id")
         print(f"Dropping submission: {submission_data}")
         return False
-    if not isinstance(submission_data["device_id"], str) or not is_valid_uuid(submission_data["device_id"]):
+    if not isinstance(submission_data["device_id"], str) or not is_valid_uuid(
+        submission_data["device_id"]
+    ):
         print("Detected invalid device id ")
         print(f"Dropping submission: {submission_data}")
         return False
@@ -104,35 +108,11 @@ def validate_submission(submission_data):
             print(f"Event_value not formatted as list in submission")
             print(f"Dropping submission: {submission_data}")
             return False
-        for event in event_list:
-            # Checks for event_type : new_process
-            if event_type == NEW_PROCESS_EVENT:
-                if not all(key in event for key in ["cmdl", "user"]):
-                    print(f"Detected invalid event key apart from 'cmdl' and 'user' in event :{event}")
-                    print(f"Dropping submission: {submission_data}")
-                    return False
-                if not isinstance(event["cmdl"], str) or not isinstance(event["user"], str):
-                    print(f"Detected invalid Cmdl or user in event:{event}")
-                    print(f"Dropping submission: {submission_data}")
-                    return False
-            # Checks for event_type : network_connection
-            elif event_type == NETWORK_CONNECTION_EVENT:
-                if not all(key in event for key in ["source_ip", "destination_ip", "destination_port"]):
-                    print(f"Detected invalid event key apart from 'source_ip', 'destination_ip' and 'destination_port' in event:{event}")
-                    print(f"Dropping submission: {submission_data}")
-                    return False
-                if not isinstance(event["source_ip"], str) or not isinstance(event["destination_ip"], str):
-                    print(f"Detected invalid source_ip or destination_ip in event:{event}")
-                    print(f"Dropping submission: {submission_data}")
-                    return False
-                if not isinstance(event["destination_port"], int):
-                    print(f"Detected invalid destination_port in event:{event}")
-                    print(f"Dropping submission: {submission_data}")
-                    return False
 
     return True
 
-def is_valid_uuid(uuid_str):
+
+def is_valid_uuid(uuid_str: str):
     """
     Validation function for uuid
 
@@ -148,11 +128,12 @@ def is_valid_uuid(uuid_str):
         return False
     return str(uuid_obj) == uuid_str
 
-def preprocess_submission(submission):
+
+def preprocess_submission(submission: dict):
     """
-    Preprocesses a submission and 
+    Preprocesses a submission and
     extracts individual events for publishing to Kinesis.
-    
+
     Args:
         submission : The message data from the queue to be prrocesses
 
@@ -166,29 +147,17 @@ def preprocess_submission(submission):
     events = submission.get("events", {})
     for event_type, event_list in events.items():
         for event in event_list:
-            processed_event = ProcessedEvent(event_type, submission_id, device_id, event_data=event)
-            # processed_event = {
-            #     "event_type": "new_process | network_connection",
-            #     "event_id": str(uuid.uuid4()),
-            #     "submission_id": submission_id,
-            #     "device_id": device_id,
-            #     "time_processed": datetime.utcnow().isoformat(),
-            #     "event_data": event
-            # }
-            # if event_type == "new_process":
-            #     processed_event['event_type']: "new-process"
-            # elif event_type == "network_connection":
-            #     processed_event['event_type']: "network_connection"
-            
-
-            processed_events.append(json.loads(processed_event.toJson()))
+            processed_event = ProcessedEvent(
+                event_type, submission_id, device_id, event_data=event
+            )
+            processed_events.append(processed_event)
 
     return processed_events
 
 
-def publish_to_kinesis(kinesis_client,events):
+def publish_to_kinesis(kinesis_client, events: list[ProcessedEvent]):
     """
-    Publish individual event to kinesis data stream 
+    Publish individual event to kinesis data stream
 
     Args:
         events         : Events
@@ -198,13 +167,14 @@ def publish_to_kinesis(kinesis_client,events):
             kinesis_client.put_record(
                 StreamName=STREAM_NAME,
                 Data=json.dumps(event),
-                PartitionKey=event['submission_id']
+                PartitionKey=event["submission_id"],
             )
-        
+
     except (EndpointConnectionError, ClientError) as e:
-        pass 
-    
-def delete_from_queue(sqs_client,queue_url,receipt_handle):
+        pass
+
+
+def delete_from_queue(sqs_client, queue_url: str, receipt_handle):
     """
     Delete message from Queue
 
@@ -213,62 +183,78 @@ def delete_from_queue(sqs_client,queue_url,receipt_handle):
         queue_url       : The URL of the Queue
         receipt_handle  : Unique identifier associated with a message from SQS
     """
-    try:        
-        sqs_client.delete_message(
-            QueueUrl=queue_url,
-            ReceiptHandle=receipt_handle
-        )
+    try:
+        sqs_client.delete_message(QueueUrl=queue_url, ReceiptHandle=receipt_handle)
     except (EndpointConnectionError, ClientError) as e:
         pass
+
 
 def main():
     """
     Continuously processes submissions from SQS and publishes processed events to Kinesis.
     """
-    
+
     # initialize AWS Clients
-    sqs_client = boto3.client('sqs', config=config, endpoint_url=AWS_ENDPOINT_URL, verify=False)
-    kinesis_client = boto3.client('kinesis', config=config, endpoint_url=AWS_ENDPOINT_URL, verify=False)
-    
+    sqs_client = boto3.client(
+        "sqs", config=config, endpoint_url=AWS_ENDPOINT_URL, verify=False
+    )
+    kinesis_client = boto3.client(
+        "kinesis", config=config, endpoint_url=AWS_ENDPOINT_URL, verify=False
+    )
+
     while True:
         try:
-            queue_url = sqs_client.get_queue_url(QueueName='submissions')['QueueUrl']
-            # print(f"trying to connect to receive message from sqs : {queue_url}")
+            queue_url = sqs_client.get_queue_url(QueueName="submissions")["QueueUrl"]
             print("Receive submission called")
             # Receive messages from SQS
-            messages = receive_submissions(sqs_client, queue_url, BATCH_SIZE, VISIBILITY_TIMEOUT)
-        
+            messages = receive_submissions(
+                sqs_client, queue_url, BATCH_SIZE, VISIBILITY_TIMEOUT
+            )
+
             for message in messages:
                 # Process each message
-                encoded_body = message['Body']
+                encoded_body = message["Body"]
                 # Decode the base64-encoded message body
                 decoded_body = base64.b64decode(encoded_body)
                 # Deserialize the message from JSON
                 message_data = json.loads(decoded_body)
-                receipt_handle = message['ReceiptHandle']
-                
+                receipt_handle = message["ReceiptHandle"]
+
                 # validate submission
-                if not validate_submission(message_data):                    
-                    delete_from_queue(sqs_client,queue_url,receipt_handle)
-                    # print(f"Deleted invalid submission data from queue: {message_data}")                    
+                if not validate_submission(message_data):
+                    # Improvement Idea: Alternatively move the invalid submission to dead letter queues
+                    delete_from_queue(sqs_client, queue_url, receipt_handle)
+                    print(f"Dropped invalid submission data from queue: {message_data}")
                     continue
 
                 processed_events = preprocess_submission(submission=message_data)
 
                 if processed_events:
+                    # validate each processed event
+                    valid_events = [
+                        event for event in processed_events if event.validate_event()
+                    ]
+
+                    valid_events_dict = [
+                        json.loads(event.toJson()) for event in valid_events
+                    ]
+
                     # Publish processed events to Kinesis stream
-                    # for event in processed_events:
-                        print("------------------------------------------")
-                        print(f"{datetime.utcnow().isoformat()} Publishing Events to Kinesis: {processed_events}")
-                        publish_to_kinesis(kinesis_client, processed_events)
+                    print("------------------------------------------")
+                    print(
+                        f"{datetime.utcnow().isoformat()} Publishing Events to Kinesis: {valid_events_dict}"
+                    )
+                    # publish_to_kinesis(kinesis_client, processed_events)
+                    publish_to_kinesis(kinesis_client, valid_events_dict)
 
                 # Delete the message from SQS
-                delete_from_queue(sqs_client,queue_url,receipt_handle)
+                delete_from_queue(sqs_client, queue_url, receipt_handle)
 
-                #TODO: Do something about this, remove??
+                # TODO: Do something about this, remove??
                 time.sleep(random.randint(5, 15))
         except (EndpointConnectionError, ClientError) as e:
             pass
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
--- preprocessor-service/processed_event.py
+++ preprocessor-service/processed_event.py
@@ -2,17 +2,59 @@ import uuid
 import json
 from datetime import datetime
 
+
 class ProcessedEvent:
+
+    NEW_PROCESS_EVENT = "new_process"
+    NETWORK_CONNECTION_EVENT = "network_connection"
     """
     Class representing a processed event.
     """
-    def __init__(self, event_type, submission_id, device_id, event_data):
+
+    def __init__(
+        self, event_type: str, submission_id: str, device_id: str, event_data: dict
+    ):
         self.event_id = str(uuid.uuid4())
         self.event_type = event_type
         self.submission_id = submission_id
         self.device_id = device_id
         self.time_processed = datetime.utcnow().isoformat()
         self.event_data = event_data
-        
+
     def toJson(self):
         return json.dumps(self, default=lambda o: o.__dict__)
+
+    def validate_event(self):
+        # Checks for event_type : new_process
+        if self.event_type == self.NEW_PROCESS_EVENT:
+            if not all(key in self.event_data for key in ["cmdl", "user"]):
+                print(
+                    f"Detected invalid event key apart from 'cmdl' and 'user'. Dropping event :{self.event_data}"
+                )
+                return False
+            if not isinstance(self.event_data["cmdl"], str) or not isinstance(
+                self.event_data["user"], str
+            ):
+                print(f"Detected invalid Cmdl or user. Dropping event :{self.event_data}")
+                return False
+        # Checks for event_type : network_connection
+        elif self.event_type == self.NETWORK_CONNECTION_EVENT:
+            if not all(
+                key in self.event_data
+                for key in ["source_ip", "destination_ip", "destination_port"]
+            ):
+                print(
+                    f"Detected invalid event key apart from 'source_ip', 'destination_ip' and 'destination_port' in event:{self.event_data}"
+                )
+                return False
+            if not isinstance(self.event_data["source_ip"], str) or not isinstance(
+                self.event_data["destination_ip"], str
+            ):
+                print(
+                    f"Detected invalid source_ip or destination_ip. Dropping event :{self.event_data}"
+                )
+                return False
+            if not isinstance(self.event_data["destination_port"], int):
+                print(f"Detected invalid destination_port. Dropping event :{self.event_data}")
+                return False
+            return True
